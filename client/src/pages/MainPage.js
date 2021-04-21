f509d43f5ec7a3005f8ead4f24ceeb907233288a
ashust75@gmail.com <ashust75@gmail.com>
2021-04-21 06:01:46 +0300

    Project added

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,43 @@
+import React, {useContext, useState} from "react"
+import axios from "axios";
+import {useHistory} from "react-router-dom"
+import {AuthContext} from "../context/AuthContext";
+
+export const MainPage = () =>{
+    const contxt = useContext(AuthContext)
+    const history = useHistory()
+    const [formData, setFormData] = useState({
+        file: null
+    })
+    const uploadHandler = async () => {
+        try {
+            const data = axios.post("/upload", formData.file)
+            contxt.img = (await data).data
+            history.push("/image")
+        } catch (e) {
+            console.log("uploadError")
+        }
+    }
+    const changeHandler = files => {
+        const file = files[0]
+        const formFileData = new FormData()
+        formFileData.append("file", file)
+        setFormData({...formData, "file":formFileData})
+    }
+    return (
+        <div className="bodyMain">
+            <header>
+                <span><br />Welcome to image converter!!!<br /></span>
+                <span>Here you can put filters on your images</span>
+            </header>
+            <div className="bodyContent">
+                Please select your image:
+                <input type="file" name="file" accept="image/*" onChange={(e) => changeHandler(e.target.files)}></input>
+                    <button onClick={uploadHandler}>Enter</button>
+            </div>
+            <footer>
+                @Copyright 2021. All rights reserved.
+            </footer>
+        </div>
+    )
+}
